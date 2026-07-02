c3d369a2d1fba02359fd6b15354d4a9cfdf3c29f
lia <silviamaliaed@gmail.com>
2026-07-02 20:29:46 +0700

    revert: cancel Midtrans payment integration and keep Google OAuth config

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,11 +6,10 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory;
+    use HasApiTokens;
 
     protected $primaryKey = 'id_user';
 
